4942836b13638dc81020c7627d57d5cc71662d03
Bruce Wong <brucewong21@gmail.com>
2022-11-06 20:42:50 +0000

    Update 2 files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,53 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function IndexPage() {
+  const headerItems = [
+    "communities",
+    "concierge",
+    "buy",
+    "sell",
+    "market update",
+    "contact",
+  ];
   return (
     <div>
-      Hello World. <Link href="/about">About</Link>
+      <div>add logo</div>
+      <nav>
+        <ul>
+          {headerItems.map((headerItem) => {
+            return (
+              <li>
+                <a href={`/${headerItem}`}>{headerItem}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+      <div>
+        <section>
+          <h2>About Alice Wu</h2>
+          <p>Stuff</p>
+          <p>Stuff</p>
+          <p>Stuff</p>
+          <p>Stuff</p>
+        </section>
+        <section>
+          <img src="" alt="Alice Wu" />
+        </section>
+      </div>
+
+      <section>
+        <h2>I am looking to</h2>
+        <div>
+          <a type="button" href="/list">
+            list your home
+          </a>
+          <a type="button" href="/buy">
+            buy a home
+          </a>
+        </div>
+      </section>
+      <div></div>
     </div>
-  )
+  );
 }
