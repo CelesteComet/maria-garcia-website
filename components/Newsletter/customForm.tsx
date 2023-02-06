42973b8e0db0089275d3e2d253e5e6a3843e3aee
Bruce Wong <brucewong21@gmail.com>
2023-02-05 19:01:33 -0800

    footer fix

--- components/Newsletter/customForm.tsx
+++ components/Newsletter/customForm.tsx
@@ -15,22 +15,26 @@ export const CustomForm = ({ status, message, onValidated }) => {
     }
 
     return (
-        <div>
+        <div className='flex-col md:flex-row'>
+            <fieldset className="flex flex-col md:inline md:mt-0">
             <label htmlFor="name">Name: </label>
             <input 
                 id="name"
                 type='text'
-                className="border border-black p-1 px-4 text-center rounded-md mr-4"
+                className="border border-black p-1 px-4 text-center rounded-md md:mr-4 mr-0 "
                 required
                 ref={node => (name = node)} />
+                </fieldset>
+                <fieldset className="flex flex-col md:inline  mt-4 md:mt-0">
             <label htmlFor="email">Email: </label>
             <input 
                 id="email"
-                className="border border-black p-1 px-4 text-center rounded-md mr-4"
+                className="border border-black p-1 px-4 text-center rounded-md md:mr-4 mr-0"
                 type='email'
                 required
                 ref={node => (email = node)} />    
-            <button className={buttonClass} onClick={submit}>Subscribe</button>
+                </fieldset>
+            <button className={`${buttonClass} mt-4 md:mt-0`} onClick={submit}>Subscribe</button>
             {status === 'error' && <p>An error occurred, please try again later.</p>}
             {status === 'sending' && <p>Sending...</p>}
             {status === 'success' && <p>Thank you for subscribing!</p>}
--- components/Newsletter/index.tsx
+++ components/Newsletter/index.tsx
@@ -7,7 +7,7 @@ export default function Newsletter(): JSX.Element {
   const url = "https://compass.us17.list-manage.com/subscribe/post?u=986b8f91313c901555be760bf&amp;id=a5caea902c&amp;f_id=00ae57e0f0";
 
   return (
-    <div className={`flex flex-col justify-center items-center p-8 bg-[#F8F6F1] ${styles.newsletter}`}>
+    <div className={`flex flex-col justify-center items-center p-8 bg-[#F8F6F1] ${styles.newsletter} text-center`}>
       <h1 className="text-3xl mt-8">
         Join The
         Exclusive
